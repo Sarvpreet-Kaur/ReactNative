9d481f37277e1f3b3b66c79f3d79251791640a1c
Sarvpreet-Kaur <sarvpreetk22@gmail.com>
2026-06-13 22:00:33 +0530

    Added Theme to CRUD App

--- ToDo_CRUD/app/_layout.tsx
+++ ToDo_CRUD/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from '@/context/ThemeContext';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -10,11 +11,13 @@ export const unstable_settings = {
 export default function RootLayout() {
 
   return (
-    <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
--- ToDo_CRUD/app/index.jsx
+++ ToDo_CRUD/app/index.jsx
@@ -1,12 +1,15 @@
 import {Text, View, TextInput, Pressable, StyleSheet, FlatList} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {data} from '@/data/todo'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import {ThemeContext} from '@/context/ThemeContext'
+import Octicons from '@expo/vector-icons/Octicons'
 
 export default function Index(){
   const [todos, setTodos] = useState([...data].sort((a,b)=> b.id-a.id))
   const [text, setText] = useState('')
+  const {colorScheme, setColorScheme, theme} = useContext(ThemeContext)
 
   const addTodo = () =>{
     if(text.trim()){
@@ -23,6 +26,8 @@ export default function Index(){
   const removeTodo = (id) =>{
     setTodos(todos.filter(todo => todo.id !== id))
   }
+
+  const styles = createStyles(theme, colorScheme)
 
   const renderItem = ({item}) => (
     <View style={styles.todoItem}>
@@ -49,6 +54,14 @@ export default function Index(){
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
+
+        <Pressable 
+          onPress={()=>setColorScheme(colorScheme==='light'? 'dark': 'light')}
+          style = {{marginLeft: 10}}>
+
+            {colorScheme === 'dark'? <Octicons name="moon" size={36} color={theme.text} selectable={undefined} style={{width: 36}}></Octicons>:
+                              <Octicons name="sun" size={36} color={theme.text} selectable={undefined} style={{width: 36}}></Octicons>}
+        </Pressable>
       </View>
 
       <FlatList 
@@ -61,61 +74,62 @@ export default function Index(){
     </SafeAreaView>
   )
 }
+  function createStyles(theme, colorScheme){
+  return StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10, 
+      padding: 10, 
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+      borderColor: 'white'
+    },
+    input:{
+      flex: 1,
+      borderColor: 'gray',
+      borderRadius: 10,
+      marginRight: 10,
+      fontSize: 18,
+      minWidth: 0,
+      color: theme.text
+    },
+    addButton:{
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText:{
+      fontSize: 18,
+      color: colorScheme==='dark'?'black':'white'
+    },
+    todoItem:{
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 4,
+      padding: 10,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 1,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto'
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      color: theme.text
+    },
+    completedText:{
+      textDecorationLine: 'line-through'
+    }
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10, 
-    padding: 10, 
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-    borderColor: 'white'
-  },
-  input:{
-    flex: 1,
-    borderColor: 'gray',
-    borderRadius: 10,
-    marginRight: 10,
-    fontSize: 18,
-    minWidth: 0,
-    color: 'white'
-  },
-  addButton:{
-    backgroundColor: 'white',
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText:{
-    fontSize: 18,
-    color: 'black'
-  },
-  todoItem:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    gap: 4,
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto'
-  },
-  todoText: {
-    flex: 1,
-    fontSize: 18,
-    color: 'white'
-  },
-  completedText:{
-    textDecorationLine: 'line-through'
-  }
-
-})
+  })
+}
